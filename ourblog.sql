c7733ca14b482f4f44ccd7fb3484b373536d8246
zzy <alanjoe0124@163.com>
2017-03-07 16:40:38 +0800

    Add sql info

---
+++ ourblog.sql
@@ -0,0 +1,50 @@
+
+--
+-- Database: `ourblog`
+--
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `blog`
+--
+
+CREATE TABLE IF NOT EXISTS `blog` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `idx_column_id` tinyint(3) NOT NULL,
+  `title` char(60) NOT NULL,
+  `content` mediumtext,
+  `user_id` tinyint(3) NOT NULL,
+  `post_time` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=7 ;
+
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `index_column`
+--
+
+CREATE TABLE IF NOT EXISTS `index_column` (
+  `id` tinyint(3) NOT NULL AUTO_INCREMENT,
+  `name` char(20) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=8 ;
+
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `user`
+--
+
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` tinyint(3) NOT NULL AUTO_INCREMENT,
+  `email` char(40) NOT NULL,
+  `pwd` char(40) NOT NULL,
+  `reg_time` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=13 ;
+
+
