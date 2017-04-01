c358bd618720947d92cbf5718bfff9d1880ec802
zhiyuan <zhiyuan@zhiyzh.(none)>
2017-04-01 16:16:43 +0800

    Modify Session and Mysql class

--- ClassLib/Mysql.class.php
+++ ClassLib/Mysql.class.php
@@ -3,112 +3,93 @@
 Class Mysql {
 
     private $mysql;
-    static $_instance;
-    
-    private function __construct($dbConf) {    
-        $this->mysql = new pdo($dbConf['dbinfo'], $dbConf['dbUser'], $dbConf['dbPwd'], array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+    protected static $_instance;
+
+    private function __construct() {
+        $this->mysql = new pdo(
+                'mysql:host=localhost;dbname=ourblog', 'root', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION)
+        );
     }
-    
-    private function __clone(){}
-    
-    public static function getInstance($dbConf){
-        if(!(self::$_instance instanceof self)){
-            self::$_instance =new self($dbConf);
+
+    private function __clone() {
+        
+    }
+
+    public static function getInstance() {
+        if (!(self::$_instance instanceof self)) {
+            self::$_instance = new self();
         }
         return self::$_instance;
     }
-    
-    public function getLastinsertId(){
-        return $this->mysql->LastinsertId();
-    }
-    
-    public function bind_array(&$stmt,$arr=array(),$typeArray=array()){
 
-        foreach($arr as $k=>$v){
-            $stmt->bindValue(':'.$k,$v,$typeArray[$k]);
+    public function select($table, $data = array(), $where = array()) {
+        // select * from table where id=? AND name=?...
+        $column = "*";
+        if ($data != NULL) {
+            $column = implode(',', array_values($data));
         }
-        $stmt->execute();
-    }
-    
-    public function select($table,$where=array(),$col=array()){
-        // select * from table where cond1='' and cond2='';
-        $sCol="*";
-        if(isset($col)){
-            $n=0;
-            foreach($col as $v){
-                if($n==0){
-                    $sCol=$v;
-                    $n++;
-                }
-                else{
-                    $sCol=$sCol.",".$v;
-                }
+        $valuesPlaceholder = "WHERE ";
+        $n = 0;
+        foreach ($where as $v) {
+            if ($n > 0) {
+                $valuesPlaceholder .= " AND ";
             }
+            $valuesPlaceholder .= " {$v}=?";
+            $n++;
         }
-        $sWh="";
-        if(isset($where)){ 
-            $m=0;
-            foreach($where as $wk=>$wv){
-                if($m==0){
-                    $sWh=" WHERE {$wk}={$wv}";
-                    ++$m;
-                }
-                else{
-                    $sWh.=" AND "."'{$wk}'={$wv}";
-                }
-            }
-        }
-        
-        $sql="select "."$sCol"." from ".$table.$sWh;
+        $sql = "SELECT {$column} FROM {$table} {$valuesPlaceholder}";
         return $sql;
     }
-    
-    public function select_execute($sql,$arr=array(),$typeArray=array()){
-        try {
-            
-            $stmt = $this->mysql->prepare($sql);
-            if(isset($arr)){
-                $this->bind_array($stmt,$arr,$typeArray);   
-            }
-            $data = $stmt->fetchAll(PDO::FETCH_ASSOC);
-            unset($stmt);
-            return $data;
-        } catch (PDOException $e) {
-            echo $e->getMessage();
-            exit;
+
+    protected function select_execute($sql, $bind = array()) {
+        $stmt = $this->mysql->prepare($sql);
+        $stmt->execute($bind);
+        return $stmt;
+    }
+
+    public function selectAll($sql, $bind = array()) {
+        return $this->select_execute($sql, $bind)->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public function selectRow($sql, $bind = array()) {
+        return $this->select_execute($sql, $bind)->fetch(PDO::FETCH_ASSOC);
+    }
+
+    public function insert($table, $data = array()) {
+        //insert into table (v,v,v) values (?,?,?)
+        $columns = implode(',', array_keys($data));
+        $values = array_values($data);
+        $valuesPlaceholder = '?' . str_repeat(',?', count($values) - 1);
+
+        $sql = "INSERT INTO $table ($columns) VALUES ($valuesPlaceholder)";
+        $stmt = $this->mysql->prepare($sql);
+        $stmt->execute($values);
+    }
+
+    public function getLastInsertId() {
+        return $this->mysql->lastInsertId();
+    }
+
+    public function delete($table, $bind = array()) {
+        // delete from table where id = ?
+        $sql = "DELETE FROM $table WHERE id = ?";
+        $this->mysql->prepare($sql)->execute($bind);
+    }
+
+    public function update($table, $bind = array()) {
+      //  $sql = "update $table set idx_column_id = ?,title = ?,content = ?,post_time = ? where id = ?";
+        $n = 0;
+        $valuesPlaceholder="";
+        foreach ($bind as $k=>$v){
+            if($n >0){
+                $valuesPlaceholder .= ",";
+            }  
+            $valuesPlaceholder .= "$k = ?";
+            $n ++;      
         }
+        $sql = "UPDATE $table SET ". $valuesPlaceholder;
+        $val=array_values($bind);
+        $this->mysql->prepare($sql)->execute($val);
     }
-    
-    public function insert($table,$arr=array()){
-        // insert into table (col1,col2,..) values ('','',''...)
-            $m=0;
-            foreach($arr as $k=>$v){
-                if($m==0){
-                    $kstr="$k";
-                    $vstr="$v";
-                    ++$m;
-                }
-                else{
-                    $kstr.=",".$k;
-                    $vstr.=",".$v;
-                }
-            }
-          
-        $sql="insert into ".$table." (".$kstr.") values (".$vstr.")";
-        return $sql;
-    }
-    
-    public function insert_execute($sql,$arr=array(),$typeArray=array()){
-        try {   
-            $stmt = $this->mysql->prepare($sql);
-            if(isset($arr)){
-                $this->bind_array($stmt,$arr,$typeArray);   
-            }
-            unset($stmt);
-        } catch (PDOException $e) {
-            echo $e->getMessage();
-            exit;
-        }
-    }
-    
+
 }
--- ClassLib/Session.class.php
+++ ClassLib/Session.class.php
@@ -1,33 +1,20 @@
 <?php
-class Session {
- 
-    private $mysqliExt;
 
-    public function __construct($mysqliExt){
-        $this->mysqliExt=$mysqliExt;
-    }
-    
-    public function user_session_check($loginNoNeed=0){ // $loginNoNeed=0, default:view page need login
+class Session {
+
+    public function __construct() {
         session_start();
-        $userSession=$_SESSION['userEmail'];
-        if(!empty($userSession)){
-            return $userSession;
-        }else{
-            if($loginNoNeed==1){
-                return NULL;
-            }else{
-                exit("login please.");
-            }
-        }
     }
-    
-    public function session_set($email){
-        session_start();
-        $mysqliExt=$this->mysqliExt;
+
+    public function isLogin() {     
+        return isset($_SESSION['uid']);
+    }
+
+    public function session_set($email, $userDbId) {
         $_SESSION['userEmail'] = $email;
-        $expire = time() + 3600;
-        setcookie(session_name(), session_id(), $expire, "/OurBlog");
+        $_SESSION['uid'] = $userDbId;
     }
-    
+
 }
+
 ?>
