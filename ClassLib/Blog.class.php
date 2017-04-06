f4b4105cc4d7bc89e197d286728463650af65b06
zhiyuan <zhiyuan@zhiyzh.(none)>
2017-04-06 15:35:06 +0800

    Modify class Blog and BlogManage after Add Tag function

--- ClassLib/Blog.class.php
+++ ClassLib/Blog.class.php
@@ -1,19 +1,22 @@
 <?php
 
-class Blog {    
-    
-    public function authority_check($blog) 
-    {
-        $data = Mysql::getInstance()->selectRow("select id from blog where id = ? and user_id = ?",array($blog,$_SESSION['uid']));
-        if(!$data)
-        {
+class Blog {
+
+    public function authority_check($blog) { // used for BlogManage and EditBlog
+        $data = Mysql::getInstance()->selectRow("select id from blog where id = ? and user_id = ?", array($blog, $_SESSION['uid']));
+        if (!$data) {
             exit("sorry, permission denied");
         }
     }
-    
-    public function list_idx_columns()
-    {
+
+    public function list_idx_columns() { // used for WriteBlog and Index
         return Mysql::getInstance()->selectAll("select * from index_column");
     }
+
+    public function list_sys_tag() { // used for WriteBlog and EditBlog
+        return Mysql::getInstance()->selectAll("select * from tag limit 0,4");
+    }
+    
+    
 
 }
--- ClassLib/BlogManage.class.php
+++ ClassLib/BlogManage.class.php
@@ -1,13 +1,14 @@
 <?php
 
-class BlogManage extends Blog{
+class BlogManage extends Blog {
 
-	public function list_user_blog() {
+    public function list_user_blog() {
         return Mysql::getInstance()->selectAll("select * from blog where user_id = ?", array($_SESSION['uid']));
     }
 
     public function delete_blog($blogId) {
-        Mysql::getInstance()->delete("blog", array($blogId));
+        Mysql::getInstance()->delete("blog", array('id'=>$blogId));
+        Mysql::getInstance()->delete("blog_tag",array('blog_id'=>$blogId));
     }
 
     public function edit_blog($blogId) {
