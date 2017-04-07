51b152d80197b54c1bcc83cc15b8ad2e7cefb3b6
zhiyuan <zhiyuan@zhiyzh.(none)>
2017-04-07 16:15:41 +0800

    Modify Index

--- ClassLib/Index.class.php
+++ ClassLib/Index.class.php
@@ -5,9 +5,9 @@ class Index extends Blog{
     public function list_blogs($col) {
         if(!$col){
             return Mysql::getInstance()->selectAll("select * from blog");   
-        }else{
-            return Mysql::getInstance()->selectAll("select * from blog where idx_column_id=?", array($col));
         }
+        return Mysql::getInstance()->selectAll("select * from blog where idx_column_id = ? ", array($col));
+
     }
 
 }
--- index.php
+++ index.php
@@ -3,49 +3,31 @@ require_once __DIR__ . '/ClassLib/AutoLoad.php';
 ?>
 <html>
     <head>
-        <link rel="stylesheet" type="text/css" href="./common/css/main.css">
+        <meta charset="utf-8">
+        <link rel="stylesheet" type="text/css" href="/common/css/main.css">
     </head>
     <body>
         <div class="container">
             <!--content_head start-->
-            <div class="headbox">
-                <div class="head-side-box"></div>
-                <div class="head-main-box">
-                    <p><h1><a href="/index.php">OurBlog</a></h1>
-                    <?php
-                    $session = new Session();
-                    if (!$session->isLogin()) {
-                        echo '&nbsp;<h4><a href="/admin/login.php">login</a></h4>
-                                  |<h4><a href="/admin/register.php">register</a></h4>';
-                    } else {
-                        echo '&nbsp;&nbsp;<h1><a href="/admin/blog_manage.php">admin</a></h1>';
-                    }
-                    $index = new Index();
-                    $listColumns = $index->list_idx_columns();
-                    foreach ($listColumns as $value) {
-                        echo '&nbsp;<h4><a href="/index.php?col=' . $value['id'] . '">' . $value['name'] . '</a></h4>';
-                    }
-                    ?>
-                    </p>
-                    <HR width="100%">
-                </div>
-                <div class="head-side-box"></div>
-            </div>
+            <?php
+            require_once __DIR__ . '/common/html/index_head.html';
+            ?> 
             <!--content_head end->
             
             <!--contetn_body start-->
             <div class="sidebox"></div>
             <div class="mainbox">
                 <?php
-              
+                
                 if (isset($_GET['col'])) {
                     $col = filter_var($_GET['col'], FILTER_VALIDATE_INT, array(
                         'options' => array('min_range' => 1, 'max_range' => 255)));
                 } else {
                     $col = NULL;
                 }
+                $index = new Index;
                 $listBlogs = $index->list_blogs($col);
-                if ($listBlogs != NULL) {
+                if ($listBlogs) {
                     foreach ($listBlogs as $valBlg) {
                         echo '<div class="row-title">
                                         <div class="row-title-leftAlign">
@@ -53,8 +35,6 @@ require_once __DIR__ . '/ClassLib/AutoLoad.php';
                                         </div>
                                     </div>';
                     }
-                } else {
-                    echo "nothing!";
                 }
                 ?>
 
