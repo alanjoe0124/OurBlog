b661c7a4aa1c4797b2cddcff09e1db84c922c547
zhiyuan <zhiyuan@zhiyzh.(none)>
2017-04-07 16:14:01 +0800

    Modify Class Blog

--- ClassLib/Blog.class.php
+++ ClassLib/Blog.class.php
@@ -2,21 +2,25 @@
 
 class Blog {
 
-    public function authority_check($blog) { // used for BlogManage and EditBlog
+    public function authority_check($blog) { // used by BlogManage and EditBlog
         $data = Mysql::getInstance()->selectRow("select id from blog where id = ? and user_id = ?", array($blog, $_SESSION['uid']));
         if (!$data) {
             exit("sorry, permission denied");
         }
     }
 
-    public function list_idx_columns() { // used for WriteBlog and Index
+    public static function list_columns() { // used by WriteBlog and Index
         return Mysql::getInstance()->selectAll("select * from index_column");
     }
 
-    public function list_sys_tag() { // used for WriteBlog and EditBlog
+    public function list_recommend_tag() { // used by WriteBlog and EditBlog
         return Mysql::getInstance()->selectAll("select * from tag limit 0,4");
     }
     
-    
+    public function list_blog_detail($blogId) {  //used by EditBlog and BlogDetail
+        if(isset($blogId)){
+            return Mysql::getInstance()->selectRow("select * from blog where id = ?", array($blogId));
+        }
+    }
 
 }
