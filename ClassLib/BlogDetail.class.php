245cc2ef70de6f9800da2b8d9cca011d5b6225a4
zhiyuan <zhiyuan@zhiyzh.(none)>
2017-04-07 16:15:01 +0800

    Modify BlogDetail

--- ClassLib/BlogDetail.class.php
+++ ClassLib/BlogDetail.class.php
@@ -1,19 +1,14 @@
 <?php
 
 class BlogDetail extends Blog{
-
-    public function list_blog_detail() {
-        if(isset($_GET['blog'])){
-            return Mysql::getInstance()->selectAll("select * from blog where id=?", array($_GET['blog']));
-        }
-    }
     
-    public function list_blog_tag(){
-        if(isset($_GET['blog'])){
-            return Mysql::getInstance()->selectAll("select tag_name from blog_tag "
-                    . "left join tag "
-                    . "on blog_tag.tag_id = tag.id "
-                    . "where blog_id = ?", array($_GET['blog']));
+    public function list_blog_tag($blogId){
+        if(isset($blogId)){
+            return Mysql::getInstance()->selectAll("
+                    select tag_name from blog_tag 
+                    join tag 
+                    on blog_tag.tag_id = tag.id
+                    where blog_id = ?", array($blogId));
         }
     }
 }
--- blog_detail.php
+++ blog_detail.php
@@ -1,6 +1,15 @@
-<?php 
-require_once __DIR__."/ClassLib/AutoLoad.php";
+<?php
+require_once __DIR__ . "/ClassLib/AutoLoad.php";
 
+if (!isset($_GET['blog'])) {
+    exit('Blog not found');
+}
+$blogId = filter_var($_GET['blog'], FILTER_VALIDATE_INT, array(
+    'options' => array('min_range' => 1, 'max_range' => 4294967295)
+        ));
+if (!$blogId) {
+    exit('Invalid blog');
+}
 ?>
 <html>
     <head>
@@ -10,61 +19,40 @@ require_once __DIR__."/ClassLib/AutoLoad.php";
     <body>
         <div class="container">
             <!--content_head start-->
-            <div class="headbox">
-                <div class="head-side-box"></div>
-                <div class="head-main-box">
-                    <p><h1><a href="/index.php">OurBlog</a></h1>
-                    <?php
-                       $session = new Session();
-                        if (!$session->isLogin()) {
-                            echo '&nbsp;<h4><a href="/admin/login.php">login</a></h4>
-                                   |<h4><a href="/admin/register.php">register</a></h4>';
-                        }else{
-                             echo '&nbsp;&nbsp;<h1><a href="/admin/blog_manage.php">admin</a></h1>'; 
-                        }
-                        $blogDetail = new BlogDetail();
-                        $listColumns = $blogDetail->list_idx_columns();
-                        foreach($listColumns as $value){
-                            echo '&nbsp;<h4><a href="index.php?col='.$value['id'].'">'.$value['name'].'</a></h4>';
-                        }
-                    ?>
-                    </p>
-                    <HR width="100%">
-                </div>
-                <div class="head-side-box"></div>
-            </div>
+            <?php
+            require_once __DIR__ . '/common/html/index_head.html';
+            ?>    
             <!--content_head end->
-            
+                
             <!--contetn_body start-->
             <div class="sidebox"></div>
             <div class="mainbox">
                 <?php
-                    $tagNameString="";
-                    foreach($blogDetail->list_blog_tag() as $va){
-                        $tagNameString .= '<a href="#">'.$va['tag_name'].'</a>&nbsp;&nbsp;';
+                $blogDetail = new BlogDetail;
+                $tagNameString = "";
+                $blogInfo = $blogDetail->list_blog_detail($blogId);
+                $blogTags = $blogDetail->list_blog_tag($blogId);
+                if ($blogTags) {
+                    foreach ($blogTags as $va) {
+                        $tagNameString .= '<a href="#">' . $va['tag_name'] . '</a>&nbsp;&nbsp;';
                     }
-                    foreach($blogDetail->list_blog_detail() as $val){
-                        if(!empty($tagNameString)){
-                        echo '<div class="row-title-leftAlign"><h2>'
-                                .htmlspecialchars($val['title'])
-                                .'</h2>tags:<p>'
-                                .$tagNameString
-                                .'</p></div><div class="row-content">'
-                                .htmlspecialchars($val['content']).'</div>';
-                      }else{
-                        echo '<div class="row-title-leftAlign"><h2>'
-                                .htmlspecialchars($val['title'])
-                                .'</h2></div><div class="row-content">'
-                                .htmlspecialchars($val['content']).'</div>';  
-                      }
-                        
-                        }
-
+                    echo '<div class="row-title-leftAlign"><h2>'
+                    . htmlspecialchars($blogInfo['title'])
+                    . '</h2>tags:<p>'
+                    . $tagNameString
+                    . '</p></div><div class="row-content">'
+                    . htmlspecialchars($blogInfo['content']) . '</div>';
+                } else {
+                    echo '<div class="row-title-leftAlign"><h2>'
+                    . htmlspecialchars($blogInfo['title'])
+                    . '</h2></div><div class="row-content">'
+                    . htmlspecialchars($blogInfo['content']) . '</div>';
+                }
                 ?>
-                
+
             </div>
             <div class="sidebox"></div>
-             <!--contetn_body end-->
+            <!--contetn_body end-->
         </div>
     </body>
 </html>
