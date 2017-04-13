276d36a26696f3a906a90348e2ca7a1e4c09dd3d
zhiyuan <zhiyuan@zhiyzh.(none)>
2017-04-13 14:28:07 +0800

    Modify tag function on EditBlog

--- ClassLib/EditBlog.class.php
+++ ClassLib/EditBlog.class.php
@@ -7,34 +7,55 @@ class EditBlog extends Blog {
     }
 
     public function return_blog_tag($blogId) {
-
         return Mysql::getInstance()->selectAll(
-               "
+                        "
                SELECT tag_id,tag_name FROM blog_tag 
                JOIN tag
                ON blog_tag.tag_id = tag.id
                WHERE blog_tag.blog_id = ?
                 ", array($blogId)
-                );
+        );
     }
 
-    public function delete_blog_all_tag($blogId) {
-        Mysql::getInstance()->delete('blog_tag', array('blog_id' => $blogId));
-    }
+    public function update_blog_tag(SplQueue $q, $tagIdArr = array(), $blogId) {
+        $oldBlogTag = array();
+        if ($rs = $this->return_blog_tag($blogId)) {
+            foreach ($rs as $vll) {
+                $oldBlogTag[] = $vll['tag_id'];
+            }
+        }
+        if (count($oldBlogTag) < count($tagIdArr)) {
+            $blogTagQueque = $this->queque_tag($q, $tagIdArr, $oldBlogTag, 5);
+            $tagQuequeCount = $blogTagQueque->count();
+            for ($i = 0; $i < $tagQuequeCount; $i++) {
+                $blogTagArr[] = $blogTagQueque->dequeue();
+            }
 
-    public function add_recommend_tag($val, $blogId) {
-        Mysql::getInstance()->insert('blog_tag', array('blog_id' => $blogId, 'tag_id' => $val));
-    }
-
-    public function add_custom_tag($vl, $blogId) {
-        $res = Mysql::getInstance()->selectRow('select * from tag where tag_name = ?', array($vl));
-        if (!$res) {
-            Mysql::getInstance()->insert('tag', array('tag_name' => $vl));
-            Mysql::getInstance()->insert('blog_tag', array('blog_id' => $blogId,
-                'tag_id' => Mysql::getInstance()->getLastInsertId()));
+            // delete old no dup tagId when tags capacity become 5
+            if ($tagQuequeCount >= 5) {
+                foreach ($oldBlogTag as $value) {
+                    if (!in_array($value, $blogTagArr)) {
+                        Mysql::getInstance()->delete("blog_tag", array('blog_id' => $blogId, 'tag_id' => $value));
+                    }
+                }
+            }
+            // insert new blog tag when it is not dup to the old
+            foreach ($blogTagArr as $vv) {
+                if (!in_array($vv, $oldBlogTag)) {
+                    Mysql::getInstance()->insert("blog_tag", array('blog_id' => $blogId, 'tag_id' => $vv));
+                }
+            }
         } else {
-            Mysql::getInstance()->insert('blog_tag', array('blog_id' => $blogId,
-                'tag_id' => $res['id']));
+            foreach ($oldBlogTag as $valu) {
+                if (!in_array($valu, $tagIdArr)) {
+                    Mysql::getInstance()->delete("blog_tag", array('blog_id' => $blogId, 'tag_id' => $valu));
+                }
+            }
+            foreach ($tagIdArr as $vlue) {
+                if (!in_array($vlue, $oldBlogTag)) {
+                    Mysql::getInstance()->insert("blog_tag", array('blog_id' => $blogId, 'tag_id' => $vlue));
+                }
+            }
         }
     }
 
--- admin/edit_blog.php
+++ admin/edit_blog.php
@@ -7,7 +7,7 @@ try {
         exit;
     }
     if (!isset($_GET['blog'])) {
-        throw new InvalidArgumentException("UNDEFINED BLOG");
+        throw new InvalidArgumentException("Invalid blog");
     }
     $blogId = filter_var($_GET['blog'], FILTER_VALIDATE_INT, array(
         'options' => array('min_range' => 1, 'max_range' => 4294967295)
@@ -21,107 +21,62 @@ try {
 
 $editBlog = new EditBlog();
 $editBlog->authority_check($blogId);
+
+require_once __DIR__ . '/../common/front/admin_common.php';
 ?>
-<html>
-    <?php
-    require_once __DIR__ . '/../common/html/admin_head.html';
-    ?>
 
-    <!--contetn_body start-->
-    <div class="sidebox"></div>
-    <div class="mainbox">
-        <form  method="post" action="edit_blog_handle.php">
+<!--contetn_body start-->
+<div class="mainbox">
+    <form  method="post" action="edit_blog_handle.php">
 
-            <div class="row-title">
-                column:
-                <select name="column">
-                    <?php
-                    $blogInfo = $editBlog->list_blog_detail($blogId);
-                    $columns = $editBlog->list_columns();
-                    foreach ($columns as $key => $value) {
-                        if ($blogInfo['idx_column_id'] == $value['id']) {
-                            echo '<option value="' . $value['id'] . '" selected="selected"> ' . $value['name'] . '</option>';
-                        } else {
-                            echo '<option value="' . $value['id'] . '" > ' . $value['name'] . '</option>';
-                        }
-                    }
-                    ?>
-                </select>
-
-            </div>
-            <div class="row-title">
-                title:<input type="text"  id="title" name="title"  value="<?php echo htmlspecialchars($blogInfo['title']); ?>" >
-            </div>
-            <div>
-            URL:(Add it in right format, or ignore it)
-            </div>
-            <div class="row-title">
-                <input type="text" name="blog_url" value="<?php 
-                if(!empty($blogInfo['blog_url'])){
-                    echo htmlspecialchars($blogInfo['blog_url']);
-                }else{
-                    echo "http://";
-                }
-                
-                ?>">
-            </div>
-            <div class="row-text">
-                text:<textarea name="content" rows = "10"> <?php echo htmlspecialchars($blogInfo['content']); ?></textarea>
-            </div>
-            <div>
-            recommend tag:
-            </div>
-            <div class="row-title">
+        <div class="row-title">
+            column:
+            <select name="column">
                 <?php
-                $recommendTag = $editBlog->list_recommend_tag();
-                $blogTags = $editBlog->return_blog_tag($blogId);
-                foreach ($recommendTag as $vlue) {
-                    $recommendTagArr[$vlue['id']] = $vlue['tag_name'];
-                }
-                $blogTagArr = array();
-                if($blogTags){
-                    foreach ($blogTags as $val) {
-                        $blogTagArr[] = $val['tag_name'];
-                    }    
-                }
-                foreach ($recommendTagArr as $key => $vl) {
-
-                    if (!empty($blogTagArr)) {
-                        if (in_array($vl, $blogTagArr)) {// if sys tag in blog tags, checked = true; else no checked                      
-                            echo '<label><input name="recommend_tag[]" checked="true"  type="checkbox" value="' . $key . '"/>' . $vl . "</label>";
-                        } else {
-                            echo '<label><input name="recommend_tag[]"   type="checkbox" value="' . $key . '"/>' . $vl . "</label>";
-                        }
+                $blogInfo = $editBlog->list_blog_detail($blogId);
+                $columns = $editBlog->list_columns();
+                foreach ($columns as $value) {
+                    if ($blogInfo['idx_column_id'] == $value['id']) {
+                        echo '<option value="' . $value['id'] . '" selected="selected"> ' . $value['name'] . '</option>';
                     } else {
-                        echo '<label><input name="recommend_tag[]"   type="checkbox" value="' . $key . '"/>' . $vl . "</label>";
+                        echo '<option value="' . $value['id'] . '" > ' . $value['name'] . '</option>';
                     }
                 }
                 ?>
-            </div>
-            <p>custom tag:(input tags separated with space)</p>
-            <div class="row-tags">
-                <textarea name="custom_tags" rows = "7"  value="">
-                    <?php
-                    if (!empty($blogTagArr)) {
-                        foreach ($blogTagArr as $v) {
-                            if (!in_array($v, $recommendTagArr)) {
-                                echo htmlspecialchars($v) . ' ';
-                            }
-                        }
-                    }
-                    ?>
-                </textarea>
-            </div>
-            <div class="row-title">
-                <input type="hidden" name='blog' value="<?php echo $blogId; ?>">
-                <input type="submit" name='submit' value="submit">
-            </div>   
-        </form>
+            </select>
 
-    </div>
-    <div class="sidebox"></div>
-    <!--contetn_body end-->
-</div>
-</body>
-</html>
+        </div>
+        <div class="row-title">
+            title:<input type="text"  id="title" name="title"  value="<?php echo htmlspecialchars($blogInfo['title']); ?>" >
+        </div>
+        <input id="checkURL" type="checkbox" >Add URL?
+        <div id="url" class="row-title">
+            URL<input type="text" name="blog_url" placeholder="http://" value="<?php
+            if (!empty($blogInfo['blog_url'])) {
+                echo htmlspecialchars($blogInfo['blog_url']);
+            }
+            ?>">
+        </div>
+        <div id="content" class="row-text">
+            text:<textarea name="content" rows = "10"  placeholder="text..."><?php echo htmlspecialchars($blogInfo['content']); ?></textarea>
+        </div>
+        <div>
+            current tags:
+        </div>
+        <div class="row-title">
+            <?php
+            $blogTags = $editBlog->return_blog_tag($blogId); 
+            if ($blogTags) {
+                foreach ($blogTags as $val) {
+                    echo '<label><input name="current_tag[]"  checked="true" type="checkbox" value="'.$val['tag_name'].'"/>'.$val['tag_name']."</label>";
+                }
+            }
+            ?>
+        </div> 
+        
+        <input type="hidden" name='blog' value="<?php echo $blogId; ?>">
+<?php
+        $blogExt = $editBlog;
+        require_once __DIR__ . '/../common/front/form_bottom.php';
+?>
 
